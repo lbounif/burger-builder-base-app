a66a483b4fe52bbb62ad6cc8b56081896c7e1e7e
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-05-26 11:03:44 +0100

    Changing folder structure2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuiler from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
